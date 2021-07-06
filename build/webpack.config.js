860eb21bf43412217882e998ed377a02e1cf2dd1
lichaojie <chaojie.lee@outlook.com>
2021-07-06 22:30:05 +0800

    配置webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,52 +1,58 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production' // 是否生产环境
+const isProd = process.env.NODE_ENV === 'production'; // 是否生产环境
 
 function resolve(dir) {
-	return path.resolve(__dirname, '..', dir)
+  return path.resolve(__dirname, '..', dir);
 }
 
 module.exports = {
-	mode: isProd ? 'production' : 'development',
-	entry: {
-		app: './src/main.ts'
-	},
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    app: './src/main.ts',
+  },
 
-	output: {
-		path: resolve('dist'),
-		filename: '[name].[contenthash:8].js'
-	},
+  output: {
+    path: resolve('dist'),
+    filename: 'js/[name].[contenthash:8].js',
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				include: [resolve('src')]
-			}
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: [resolve('src')],
+      },
+    ],
+  },
 
-	plugins: [
-		new CleanWebpackPlugin({}),
+  plugins: [
+    new CleanWebpackPlugin({}),
 
-		new HtmlWebpackPlugin({
-			template: './public/index.html'
-		})
-	],
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: 'body'
+    }),
 
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js']
-	},
+    new ESLintPlugin({
+      fix: true,
+    }),
+  ],
 
-	devtool: isProd ? 'cheap-module-source-map' : 'eval-cheap-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
 
-	devServer: {
-		host: 'localhost', // 主机名
-		stats: 'errors-only', // 打包日志输出输出错误信息
-		port: 8088,
-		open: true
-	}
-}
+  devtool: isProd ? 'cheap-module-source-map' : 'eval-cheap-source-map',
+
+  devServer: {
+    host: 'localhost', // 主机名
+    stats: 'errors-only', // 打包日志输出输出错误信息
+    port: 8088,
+    open: true,
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-document.write('hello,ts')
+class User {
+    fullName: string
+    firstName: string
+    lastName: string
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName
+      this.lastName = lastName
+      this.fullName = firstName + ' ' + lastName
+    }
+  }
+  
+  interface Person {
+    firstName: string
+    lastName: string
+  }
+  
+  function greeter(person: Person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName
+  }
+  
+  let user = new User('Yee', 'Huang')
+  
+  console.log(greeter(user))
