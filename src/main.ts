315ffa903b5e1503de75559504fee3c8a9e341e9
lichaojie <chaojie.lee@outlook.com>
2021-08-19 21:59:57 +0800

    目录增加序号

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@
  * https://huaxhe.gitee.io/vue3_study_docs/
  */
 
-// import './接口/01_基础类型'
-// import './接口/02_接口'
-// import './接口/03_函数类型'
-// import './接口/04_类的类型'
+// import './01_接口/01_基础类型'
+// import './01_接口/02_接口'
+// import './01_接口/03_函数类型'
+// import './01_接口/04_类的类型'
 
-// import './类/01_基本示例'
-// import './类/02_继承'
-// import './类/03_多态'
-// import './类/04_修饰符'
-// import './类/05_readonly修饰符'
-// import './类/06_存取器'
-// import './类/07_静态成员'
-import './类/08_抽象类'
+// import './02_类/01_基本示例'
+// import './02_类/02_继承'
+// import './02_类/03_多态'
+// import './02_类/04_修饰符'
+// import './02_类/05_readonly修饰符'
+// import './02_类/06_存取器'
+// import './02_类/07_静态成员'
+import './02_类/08_抽象类'
