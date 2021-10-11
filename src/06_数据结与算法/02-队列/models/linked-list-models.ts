5a939f3ac719a83f6c8f628f37d06f2e79e575b6
lichaojie <chaojie.lee@outlook.com>
2021-10-11 11:02:46 +0800

    链表助手类: 完善注释

--- src/06_数据结与算法/02-队列/models/linked-list-models.ts
+++ src/06_数据结与算法/02-队列/models/linked-list-models.ts
@@ -1,17 +1,21 @@
-/**
- * 助手类
- */
+// 链表助手类
 
 
 /**
- * 添加到链表中的项
- * @param element 加入链表元素的值
- * @param next 指向链表中下一个元素的指针
+ * 添加到链表中的元素
+ * @param element 加入链表的元素
+ * @param next 链表下一个元素的指针
  */
 export class Node<T> {
   constructor(public element: T, public next?: Node<T>) {}
 }
 
+/**
+ * 添加到双向链表中的元素
+ * @param element 加入链表的元素
+ * @param prev 链表上一个元素的指针
+ * @param next 链表下一个元素的指针
+ */
 export class DoublyNode<T> extends Node<T> {
   constructor(
       public element: T,
